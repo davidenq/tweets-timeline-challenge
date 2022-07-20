70fade0db13c81b70e6431b53b49dd0a08b8858d
David <david.nunez.dev@gmail.com>
2022-07-20 07:06:23 -0500

    add composition root

---
+++ app/bootstrap.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/davidenq/tweets-timeline-challenge/app/config"
+	"github.com/davidenq/tweets-timeline-challenge/app/domain"
+	"github.com/davidenq/tweets-timeline-challenge/app/domain/usecases"
+	"github.com/davidenq/tweets-timeline-challenge/app/services/db/dynamo"
+	"github.com/davidenq/tweets-timeline-challenge/app/services/transport/http"
+	"github.com/davidenq/tweets-timeline-challenge/app/ui/api"
+	"github.com/davidenq/tweets-timeline-challenge/app/ui/api/handlers"
+	"github.com/rs/zerolog/log"
+)
+
+type App struct {
+	config          config.Config
+	dbClient        dynamodb.Client
+	httpClient      domain.HttpClientDrivenPort
+	repository      domain.RepositoryDrivenPort
+	timelineUsecase domain.TimeLineUsecaseFacadeDrivingPort
+	userUsecase     domain.UserUsecaseDrivingPort
+	handlers        handlers.HandlerDefinition
+}
+
+func (a *App) LoadConfig() *App {
+	a.config = config.LoadConfig()
+	return a
+}
+
+func (a *App) LoadServices() *App {
+	a.httpClient = http.NewClient()
+	a.dbClient = dynamo.NewConn(a.config)
+	a.repository = dynamo.NewRepository(a.dbClient)
+	return a
+}
+
+func (a *App) LoadDomain() *App {
+	a.timelineUsecase = usecases.NewTimeLineUsecase(a.httpClient, a.repository, a.config)
+	a.userUsecase = usecases.NewUserUsecase(a.repository, a.config)
+	return a
+}
+func (a *App) LoadAPI() *App {
+	a.handlers = handlers.NewHandlers(
+		a.timelineUsecase,
+		a.userUsecase,
+	)
+	return a
+}
+func (a *App) Init() {
+	log.Logger = log.With().Caller().Logger()
+	api.NewServer(a.handlers)
+}
---
+++ app/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	ENV string `env:"ENV"`
+
+	DynamoDBDomain string `env:"DYNAMODB_DOMAIN"`
+	DynamoDBPort   string `env:"DYNAMODB_PORT"`
+
+	AWSAccessKeyID     string `env:"AWS_ACCESS_KEY_ID"`
+	AWSSecretAccessKey string `env:"AWS_SECRET_ACCESS_KEY"`
+	AWSDefaultRegion   string `env:"AWS_DEFAULT_REGION"`
+
+	TwitterAPIKey       string `env:"TWITTER_API_KEY"`
+	TwitterAPISecretKey string `env:"TWITTER_API_SECRET_KEY"`
+	TwitterOAuthURL     string `env:"TWITTER_OAUTH_URL"`
+	TwitterUsersURL     string `env:"TWITTER_USERS_URL"`
+}
+
+func LoadConfig() Config {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Panicf("%+v\n", err)
+	}
+	return cfg
+}
