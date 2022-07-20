6db68a4fe91b72dfaf7602527aa3210749e2c868
David <david.nunez.dev@gmail.com>
2022-07-20 07:06:34 -0500

    add integration test

---
+++ tests/integration/integration_test.go
@@ -0,0 +1,37 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/davidenq/tweets-timeline-challenge/app/config"
+	"github.com/davidenq/tweets-timeline-challenge/app/domain/usecases"
+	"github.com/davidenq/tweets-timeline-challenge/app/services/db/dynamo"
+	"github.com/davidenq/tweets-timeline-challenge/app/services/transport/http"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDynamoDBTwitterIntegration(t *testing.T) {
+	cfg := config.LoadConfig()
+	dbConn := dynamo.NewConn(cfg)
+	httpClient := http.NewClient()
+	repository := dynamo.NewRepository(dbConn)
+	migrateUsecases := usecases.NewMigrate(repository)
+	oauthUsecase := usecases.NewOAuth(cfg, httpClient, repository)
+
+	t.Run("should create tables in DynamoDB service", func(t *testing.T) {
+		actualErr := migrateUsecases.CreateTables()
+		assert.NoError(t, actualErr)
+	})
+
+	t.Run("should get a token from Twitter service and store in DynamoDB", func(t *testing.T) {
+		token, actualErr := oauthUsecase.GetToken(context.TODO())
+		assert.NotNil(t, token)
+		assert.NoError(t, actualErr)
+	})
+
+	t.Run("should destroy tables in DynamoDB service", func(t *testing.T) {
+		actualErr := migrateUsecases.DeleteTables()
+		assert.NoError(t, actualErr)
+	})
+}
